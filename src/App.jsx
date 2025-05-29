21b82d820e877f995836571b1b9f27519bafc2f8
master-dhanesh <dhanesh1296@gmail.com>
2025-05-29 20:31:14 +0530

    API Integrtion

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import Navbar from "./components/Navbar";
-import Mainroutes from "./routes/Mainroutes";
+import axios from "./utils/axios";
 
 const App = () => {
+    const GetProducts = async () => {
+        try {
+            const res = await axios.get("/products");
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
     return (
-        <div className="font-thin py-10 px-[10%]">
-            <Navbar />
-            <Mainroutes />
+        <div className="font-thin px-[10%] py-10">
+            <h1>Axios</h1>
+            <button onClick={GetProducts}>Get Product</button>
         </div>
     );
 };
