faf4d761592ff0f2330341903027312156110ad9
master-dhanesh <dhanesh1296@gmail.com>
2025-06-06 20:12:26 +0530

    reduc 001

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import { Link, Route, Routes } from "react-router-dom";
-import Home from "./components/Home";
-import Products from "./components/Products";
+import { useSelector } from "react-redux";
 
 const App = () => {
-    return (
-        <div className="font-thin px-[10%] py-10">
-            <nav className="flex gap-x-10 mb-10">
-                <Link to="/">Home</Link>
-                <Link to="/products">Products</Link>
-            </nav>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/products" element={<Products />} />
-            </Routes>
-        </div>
-    );
+    const data = useSelector((state) => state);
+    console.log(data);
+    return <div>App</div>;
 };
 
 export default App;
